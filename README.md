a22a8925c90c0c48999c0ecd9ca20f223fa6be27
danchu0214 <naeun5794@gmail.com>
2024-06-04 17:59:43 +0900

    240604

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
+## 240604
+-index 큰 틀 완성<br>
+-세부 수정, 영상 자료 제작을 목표로함
+
 ## 240603
 
--main-title-text 스크롤 내렸을 때 사라지고, 스크롤 올렸을 때 나타나게 함
--other design부분 완성
--uxui design과 skills 완성을 목표로 함
+-main-title-text 스크롤 내렸을 때 사라지고, 스크롤 올렸을 때 나타나게 함<br>
+-other design부분 완성<br>
+-uxui design과 skills 완성을 목표로 함<br>
 
 ## 240531
 
--포트폴리오 index.html w3c 유효성 검사
--index.css 작업중
--index.js 생성
--css 완성을 목표로 함
+-포트폴리오 index.html w3c 유효성 검사<br>
+-index.css 작업중<br>
+-index.js 생성<br>
+-css 완성을 목표로 함<br>
 
 ## 240530
 
--포트폴리오 메인페이지 디자인 작업 마무리
--index.html 생성
+-포트폴리오 메인페이지 디자인 작업 마무리<br>
+-index.html 생성<br>
 -html 완성을 목표로 함
--- index.js
+++ index.js
@@ -15,3 +15,40 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const projectGroups = document.querySelectorAll(".project-group");
+    const projectWrapperNum = document.querySelector(".project-wrapper-num");
+
+    function handleScroll() {
+        let currentGroupIndex = 0;
+
+        projectGroups.forEach((projectGroup, index) => {
+            const rect = projectGroup.getBoundingClientRect();
+            if (rect.top <= 0) {
+                currentGroupIndex = index;
+            }
+        });
+
+        const spans = projectWrapperNum.querySelectorAll("span");
+        spans.forEach((span, index) => {
+            if (index === currentGroupIndex) {
+                span.classList.add("active");
+                span.style.display = "inline-block";
+            } else {
+                span.classList.remove("active");
+                span.style.display = "none";
+            }
+
+            const translateY = index === 0 ? "0px" : index === currentGroupIndex - 1 ? "-350px" : index === currentGroupIndex - 2 ? "-705px" : "0px";
+            span.style.transform = `translateY(${translateY})`;
+        });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    // 페이지 로드시 한번 실행하여 초기 상태 설정
+    handleScroll();
+});
+
+
