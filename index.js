5346b6ce255da53f98bdbbd11ab3d42609d205ab
danchu0214 <naeun5794@gmail.com>
2024-06-05 17:58:29 +0900

    240605

--- index.js
+++ index.js
@@ -1,20 +1,97 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const mainTitle = document.querySelector('.main-title');
-  const mainContents = document.querySelector('.main-contents');
 
-  window.addEventListener('scroll', function() {
-      const scrollPosition = window.scrollY;
-      const mainContentsPosition = mainContents.offsetTop;
-      const mainTitleHeight = mainTitle.offsetHeight;
+;(function ($, window, document, undefined) {
+    "use strict";
 
-      // main-contents 섹션에 도달할 때까지 main-title을 숨깁니다.
-      if (scrollPosition > mainContentsPosition - mainTitleHeight) {
-          mainTitle.classList.remove('visible'); // 스크롤을 내릴 때는 투명하게
-      } else {
-          mainTitle.classList.add('visible'); // 스크롤을 올릴 때는 나타나게
+    var ClassScrollOpacityEffect,
+        defaults = {
+            endPoint: 500,
+            opacity: 1,
+            opacityDivisor: 1000,
+            transformDivisor: 7
+        };
+
+    ClassScrollOpacityEffect = function (triggerHolder, options) {
+        return {
+            init: function () {
+                this.settings = $.extend({}, defaults, options);
+                this._effect(triggerHolder, this.settings)
+            },
+            _effect: function (holder, settings) {
+                $(window).scroll(function () {
+                    var scrollTop = $(window).scrollTop();
+
+                    if (scrollTop < settings.endPoint) {
+                        holder.css('opacity', settings.opacity - scrollTop / settings.opacityDivisor);
+                        holder.css({
+                            '-webkit-transform': 'translateY(' + scrollTop / settings.transformDivisor + '%)',
+                            '-ms-transform': 'translateY(' + scrollTop / settings.transformDivisor + '%)',
+                            transform: 'translateY(' + scrollTop / settings.transformDivisor + '%)'
+                        });
+                    }
+                });
+            }
+        };
+    };
+
+    $(document).ready(function () {
+        var scrollEffect = new ClassScrollOpacityEffect($('.main-title h2'), {
+            // 사용자 정의 옵션을 여기에 추가할 수 있습니다.
+        });
+        scrollEffect.init();
+    });
+
+})(jQuery, window, document);
+
+document.addEventListener("DOMContentLoaded", function() {
+    function smoothScrollTo(sectionId) {
+      var sectionElement = document.getElementById(sectionId);
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
       }
+    }
+  
+    var shortcuts = {
+      "list1": "Works-UIUX",
+      "list2": "Works-OTHER",
+      "list3": "SKILLS",
+      "list4": "CONTACT"
+    };
+  
+    Object.keys(shortcuts).forEach(function(shortcutId) {
+      var shortcutElement = document.getElementById(shortcutId);
+      if (shortcutElement) {
+        shortcutElement.addEventListener("click", function() {
+          smoothScrollTo(shortcuts[shortcutId]);
+        });
+      }
+    });
   });
-});
+
+  gsap.utils.toArray('.rolled-over-txt').forEach((element) => {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: '100% 100%',
+        end: '100% 100%',
+        scrub: 1
+      }
+    }).fromTo(
+      element,
+      {
+        y: 100,
+        opacity: 0
+      },
+      {
+        y: 0,
+        opacity: 1,
+        ease: 'none',
+        duration: 5
+      }
+    );
+  });
+  
+  
+  
 
 document.addEventListener("DOMContentLoaded", function() {
     const projectGroups = document.querySelectorAll(".project-group");
@@ -40,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 span.style.display = "none";
             }
 
-            const translateY = index === 0 ? "0px" : index === currentGroupIndex - 1 ? "-350px" : index === currentGroupIndex - 2 ? "-705px" : "0px";
+            const translateY = index === 0 ? "0px" : index === currentGroupIndex - 1 ? "-350px" : index === currentGroupIndex - 2 ? "-700px" : "0px";
             span.style.transform = `translateY(${translateY})`;
         });
     }
@@ -50,5 +127,3 @@ document.addEventListener("DOMContentLoaded", function() {
     // 페이지 로드시 한번 실행하여 초기 상태 설정
     handleScroll();
 });
-
-
